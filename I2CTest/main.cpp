5d712c38eff8a37ee1c826a164c202eb777ae077
dalek-raspberry-pi3-dev01 <dalek-pi@>
2017-11-20 08:57:47 +0000

    i2c

--- I2CTest/main.cpp
+++ I2CTest/main.cpp
@@ -1,17 +1,37 @@
+
 #include <iostream>
-#include <wiringPi.h>
+#include <errno.h>
+#include <wiringPiI2C.h>
 
 using namespace std;
-int main (void)
+
+// MPL115A: 50 to 115kPa, Absolute Digital Pressure Sensor
+// https://www.nxp.com/products/sensors/pressure-sensors/barometric-pressure-15-to-115-kpa/50-to-115kpa-absolute-digital-pressure-sensor:MPL115A
+
+void Test1()
 {
-  wiringPiSetup () ;
-  pinMode (0, OUTPUT) ;
-  for (;;)
-  {
-    digitalWrite (0, HIGH) ; delay (500) ;
-    cout << "ON" << endl;
-    digitalWrite (0,  LOW) ; delay (500) ;
-    cout << "OFF" << endl;
-  }
-  return 0 ;
+    for(int i=0; i<128; i++)
+    {
+        int fd = wiringPiI2CSetup(i);   // It returns a standard file descriptor.
+
+        cout << fd << endl;
+    }
+
+}
+
+int main()
+{
+   int fd, result;
+   // Initialize the interface by giving it an external device ID.
+   // The MPL115A defaults to address 0x60.
+   //
+   // It returns a standard file descriptor.
+   //
+   fd = wiringPiI2CSetup(0x60);
+   cout << fd << endl;
+
+
+
+    return 0;
+
 }
