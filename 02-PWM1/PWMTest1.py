80c57acba5e6f6c2841bf243fe540eebb096bb75
Dalek@pi3b+ <Dalek@pi3b+>
2018-11-07 22:07:04 +0900

    PWM

---
+++ 02-PWM1/PWMTest1.py
@@ -0,0 +1,23 @@
+# https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
+# https://www.raspberrypi.org/forums/viewtopic.php?t=111353
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+
+p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
+p.start(0)
+try:
+    while 1:
+        for dc in range(0, 101, 5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+        for dc in range(100, -1, -5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+p.stop()
+GPIO.cleanup()
