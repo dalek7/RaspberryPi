785c232d9125da060f7e11ec0d75855036e5e0aa
Dalek@pi3b+ <Dalek@pi3b+>
2019-09-23 18:56:52 +0900

    HTU21D

---
+++ 02-I2C-HTU21D-F/hello_htu21d.py
@@ -0,0 +1,17 @@
+# based on 
+# https://learn.adafruit.com/adafruit-htu21d-f-temperature-humidity-sensor/python-circuitpython
+
+import time
+import board
+import busio
+from adafruit_htu21d import HTU21D
+ 
+# Create library object using our Bus I2C port
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = HTU21D(i2c)
+
+
+while True:
+    print("\nTemperature: %0.1f C" % sensor.temperature)
+    print("Humidity: %0.1f %%" % sensor.relative_humidity)
+    time.sleep(2)
